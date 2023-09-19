25b1429fb429e57f9563936e676e42e771c277e3
shrikantpatel <shrikant_patelin@yahoo.com>
2023-09-19 17:37:47 -0500

    solve the island problem in python3

---
+++ _200_Number_of_Islands.py
@@ -0,0 +1,36 @@
+class Solution:
+
+    def numIslands(self, grid: list[list[str]]) -> int:
+        
+        totalRow = len(grid[0])
+        totalCol = len(grid[0])
+
+        noOfIsland = 0
+
+        for row in range(0, totalRow) :
+
+            for col in range(0, totalCol) :
+
+                noOfIsland += 1
+                self.markAllAdjacentLand(row, col, grid)
+        
+        return noOfIsland
+
+
+    def markAllAdjacentLand(self, row, col, grid:list[list[str]]) :
+        totalRow = len(grid[0])
+        totalCol = len(grid[0])
+
+        if row == totalRow : return
+        if col == totalCol : return
+        if col == -1 : return
+        if row == -1 : return
+
+        if (grid[row][col] == '1') :
+            grid[row][col] == 'X'
+            self.markAllAdjacentLand(row+1, col, grid)
+            self.markAllAdjacentLand(row, col+1, grid)
+            self.markAllAdjacentLand(row-1, col, grid)
+            self.markAllAdjacentLand(row-1, col-1, grid)
+        else :
+            return
