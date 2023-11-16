a3be86526f15f10c6ca57060ba3f16fac8efc6b9
shrikant patel <shrikant_patelin@yahoo.com>
2023-11-15 21:49:33 -0600

    solved find the peak element problem # 162

---
+++ 162_Find_Peak_Element.py
@@ -0,0 +1,23 @@
+#https://leetcode.com/problems/find-peak-element/
+
+class Solution:
+    def findPeakElement(self, nums: list[int]) -> int:
+        eakIndex = 0
+        previousIndex = 0
+        peakIndex = 0
+        lenght = len(nums)
+
+        for counter in range(1,lenght) :
+            if nums[counter-1] < nums[counter]:
+                peakIndex = counter
+
+        print (peakIndex)
+        return peakIndex
+        
+if __name__ == "__main__" :
+    t1 = Solution()
+    assert t1.findPeakElement([1,2,3,1]) == 2, 'Should be true'
+    assert t1.findPeakElement([1,2,1,3,5,6,4]) == 5, 'Should be true'
+    assert t1.findPeakElement([1]) == 0, 'Should be true'
+    assert t1.findPeakElement([1,2]) == 1, 'Should be true'
+    assert t1.findPeakElement([1,2,3]) == 2, 'Should be true'
